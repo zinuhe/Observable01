b8f7a6e581a3038a290831cff4e2d3f37cc2f514
Jimmy Saavedra <jimmy.saavedra@prodago.com>
2019-08-29 14:49:06 -0400

    Refactoring Example03

--- src/app/example03/example03.component.ts
+++ src/app/example03/example03.component.ts
@@ -10,8 +10,12 @@ export class Example03Component implements OnInit, OnDestroy {
 
   constructor() { }
 
-  // Create a susbcription object
-  private mySusbcriptions: Subscription[] = [];
+  // Store multiple subscription references in the subscriptions array and unsubscribe all of them during ngOnDestroy
+
+  // Create a susbcription object, first approach is better, second one just as example
+  private mySusbcriptions01: Subscription = new Subscription();
+  private mySusbcriptions02: Subscription[] = [];
+
 
   // Create simple observables that emits three values
   myObservable01$: Observable<number> = of(1, 2, 3);
@@ -19,16 +23,24 @@ export class Example03Component implements OnInit, OnDestroy {
 
   ngOnInit() {
     // Execute with the observer object
-    // this.mySusbcription[0] = this.myObservable01$.subscribe(x => console.log('X value: ' + x));
-    // this.mySusbcription[1] = this.myObservable02$.subscribe(y => console.log('Y value: ' + y));
+    // Built in mechanism in the Subscription itself
+    this.mySusbcriptions01.add(this.myObservable01$.subscribe(x => console.log('X value: ' + x)));
+    this.mySusbcriptions01.add(this.myObservable02$.subscribe(y => console.log('Y value: ' + y)));
 
-    this.mySusbcriptions.push(this.myObservable01$.subscribe(x => console.log('X value: ' + x)));
-    this.mySusbcriptions.push(this.myObservable02$.subscribe(y => console.log('Y value: ' + y)));
+    // Alternatives (the first one is better)
+    // this.mySusbcriptions01[0] = this.myObservable01$.subscribe(z => console.log('Z value: ' + z));
+    // this.mySusbcriptions01[1] = this.myObservable02$.subscribe(w => console.log('W value: ' + w));
+
+    this.mySusbcriptions02.push(this.myObservable01$.subscribe(z => console.log('Z value: ' + z)));
+    this.mySusbcriptions02.push(this.myObservable02$.subscribe(w => console.log('W value: ' + w)));
   }
 
   ngOnDestroy() {
     // Destruction of the component
-    this.mySusbcriptions.forEach(subscription => subscription.unsubscribe());
+    this.mySusbcriptions01.unsubscribe();
+
+    // Alternative
+    this.mySusbcriptions02.forEach(subscription => subscription.unsubscribe());
   }
 
 }
